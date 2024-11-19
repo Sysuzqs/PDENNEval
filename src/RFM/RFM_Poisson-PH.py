ccb94f042e6ace6684e7fd462d0c6617bdb03e56
zhouziyang@172.18.166.69 <zhouzy36@mail2.sysu.edu.cn>
2024-11-19 22:44:14 +0800

    Upload the implement of RFM for Poisson Equation.

---
+++ src/RFM/RFM_Poisson-PH.py
@@ -0,0 +1,177 @@
+# %%
+import argparse
+import numpy as np
+import os
+import scipy as sci
+from sklearn.metrics import max_error
+import time
+import torch
+import torch.nn as nn
+from torch import Tensor, sin
+
+# %%
+# Parse arguments
+argparser = argparse.ArgumentParser()
+argparser.add_argument("-d", "--dimension", type=int, default=3, help="The dimension of equation to be solved (default: 3).")
+argparser.add_argument("--num-basis", type=int, default=1000, help="The number of basis/feature functions (default: 1000).")
+argparser.add_argument("--scale", type=float, default=1.0, help="The scale of NN (default: 1.0).")
+argparser.add_argument("--seed", type=int, default=2024, help="The random seed (default: 2024).")
+argparser.add_argument("--save-dir", type=str, default="checkpoint/Poisson-PH", help="The directory path to save model (default: checkpoint/Poisson-PH).")
+args = argparser.parse_args()
+print(args)
+
+# Define the dimension of equation
+dimension = args.dimension
+n_basis_func = args.num_basis
+
+# %%
+# Set random seed and deterministic behavior 2024/1018/1021/2010
+seed = args.seed
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+np.random.seed(seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+torch.set_default_dtype(torch.float64)
+start_time = time.time()
+
+# %%
+class Net(nn.Module):
+    def __init__(self, hidden_size, dimension, scale):
+        # hidden_size: The hidden layer size that is equal to the number of basis/feature functions
+        super(Net, self).__init__()
+        self.input_layers = torch.nn.Linear(dimension, hidden_size)
+        self.input_layers.weight.data.uniform_(-scale, scale)
+        self.input_layers.bias.data.uniform_(-scale, scale)
+        print("self.input_layers.weight.shape:", self.input_layers.weight.shape)
+
+    def forward(self, x):
+        y = torch.tanh(self.input_layers(x))
+        return y
+
+net = Net(n_basis_func, dimension, args.scale)
+
+# %%
+# Sample boundary points
+n_bc = 400 # The number of boundary points
+xl = -1.0 # left boundary: [xl, xr]^d
+xr = 1.0 # right boundary: [xl, xr]^d
+
+points_bc_raw = np.random.uniform(xl, xr, (2*n_bc*dimension, dimension))
+for i in range(n_bc):
+    # range for this is [i*2d, (i+1)*2d]
+    for j in range(dimension):
+        points_bc_raw[i*2*dimension+j, j] = xl
+        points_bc_raw[i*2*dimension+dimension+j, j] = xr
+X_initial = torch.from_numpy(points_bc_raw)
+
+# Sample interior points
+n_in = 3000 # The number of interior points
+points_in = np.random.uniform(xl, xr, (n_in, dimension))
+X = torch.from_numpy(points_in)
+X.requires_grad_(True)
+
+# %%
+# Calculate first and second order derivatives
+D_list = [[] for _ in range (dimension)]
+DD_list = [[] for _ in range (dimension)]
+
+def compute_grad_hessian(net, X, index):
+    d = torch.autograd.grad(
+        net(X)[:, index:index+1], 
+        X, 
+        grad_outputs=torch.ones_like(net(X)[:, index:index+1]), 
+        create_graph=True)[0]  
+    for i in range (dimension):    
+        D_list[i].append(d[:, i:i+1].detach()) # D_list[i]: The first-order derivative of the i-th dimension
+        
+    # Calculate the second derivative
+    def compute_second_derivatives(d, index):
+        d2 = torch.autograd.grad(d[:, index:index+1],
+                                 X, 
+                                 grad_outputs=torch.ones_like(d[:, index:index+1]), 
+                                 create_graph=True)[0]
+        return d2[:,index:index+1].detach()
+    for i in range (dimension):
+        # DD_list[i]: The second-order derivative of the i-th dimension
+        DD_list[i].append(compute_second_derivatives(d, i))
+    
+for i in range(n_basis_func):
+    compute_grad_hessian(net, X, i)
+
+# The first derivative
+D = []
+for i in range(len(D_list)):
+    D.append(torch.stack(D_list[i]))
+D = torch.stack(D).squeeze().permute(0, 2, 1)
+
+# The second derivative
+DD = []
+for i in range(len(DD_list)):
+    DD.append(torch.stack(DD_list[i]))
+DD = torch.stack(DD).squeeze().permute(0, 2, 1)
+
+# %%
+# The expression of ground truth
+def real_solution(p: torch.Tensor):
+    sum = 0 
+    for i in range(dimension):
+        sum += p[:, i:i+1]
+    return (1/dimension*sum)**2 + torch.sin(1/dimension*sum)
+
+# The expression of boundary condition
+def boundary(p: Tensor):
+    return real_solution(p)
+
+# The expression of initial condition
+def initial(p: Tensor):
+    return real_solution(p)
+
+# %%
+f_domain_exact = np.zeros((X.shape[0], 1)) # The value of the right hand side at collocation points
+
+f_domain = 0 
+for i in range(dimension):
+    f_domain += DD[i].detach().cpu().numpy()
+
+sum_X = 0
+for i in range(dimension):
+    sum_X += X[:,i:i+1]
+
+u_initial_pred = net(X_initial)    
+u_initial_pred = u_initial_pred.detach().cpu().numpy()
+u_initial_exact = boundary(X_initial).detach().cpu().numpy()
+
+# AC = f
+A = np.vstack([f_domain + 1 / dimension * (sin(1/dimension*(sum_X)) - 2).detach().cpu().numpy(), u_initial_pred])
+f = np.vstack([f_domain_exact, u_initial_exact])
+
+# %%
+# Perform least-squares approximation to obtain coefficient matrix C
+C = sci.linalg.lstsq(A, f)[0] # (M, 1)
+
+# %%
+# Calculate Metrics
+u_pred = (net(X).detach().cpu().numpy() @ C) # Calculate prediction, net(x) is equivalent to $\psi(x) in formula (2.8) of the paper
+u_domain_exact = real_solution(X).detach().cpu().numpy() # Ground Truth of interior points
+
+# Calculate L2 relative error
+rel_error = np.linalg.norm(u_pred-u_domain_exact, 2) / np.linalg.norm(u_domain_exact, 2)
+print("L2 Relative Error:", rel_error)
+
+# Calculate max error
+Max_error = max_error(u_pred, u_domain_exact)
+print("Max Error:", Max_error)
+
+# %%
+# Record execution time
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"Execution Time: {execution_time}s")
+
+# %%
+# Save basis/feature functions and coefficient matrix C
+if not os.path.exists(args.save_dir):
+    os.makedirs(args.save_dir)
+torch.save(net.state_dict(), os.path.join(args.save_dir, f"network_{dimension}D_{args.num_basis}basis_scale{args.scale}_seed{args.seed}.pth"))
+np.save(os.path.join(args.save_dir, f"C_{dimension}D_{args.num_basis}basis_scale{args.scale}_seed{args.seed}.npy"), C)
---
+++ src/RFM/RFM_Poisson-PL.py
@@ -0,0 +1,172 @@
+# %%
+import argparse
+import numpy as np
+import os
+import scipy as sci
+import torch
+import torch.nn as nn
+from sklearn.metrics import max_error
+import pandas as pd
+import time
+
+# %%
+# Parse arguments
+argparser = argparse.ArgumentParser()
+argparser.add_argument("--num-basis", type=int, default=800, help="The number of basis/feature functions (default: 800).")
+argparser.add_argument("--scale", type=float, default=0.1, help="The scale of NN (default: 0.1).")
+argparser.add_argument("--seed", type=int, default=2024, help="The random seed (default: 2024).")
+argparser.add_argument("--save-dir", type=str, default="checkpoint/Poisson-PL", help="The directory path to save model (default: checkpoint/Poisson-PL).")
+args = argparser.parse_args()
+print(args)
+
+n_basis_func = args.num_basis  # define the number of feature functions
+
+# %%
+# Set random seed and deterministic behavior
+seed = args.seed
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+np.random.seed(seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+torch.set_default_dtype(torch.float64)
+start_time = time.time()
+
+# %%
+# Define the network architecture
+class Net(nn.Module):
+    def __init__(self, hidden_size, scale):
+        # hidden_size: The hidden layer size that is equal to the number of basis/feature functions
+        super(Net, self).__init__()
+        self.input_layers = torch.nn.Linear(2, hidden_size)
+        self.input_layers.weight.data.uniform_(-scale, scale)
+        self.input_layers.bias.data.uniform_(-scale, scale)
+        print("The shape of input layer weights:", self.input_layers.weight.shape)
+
+    def forward(self, x):
+        y = torch.tanh(8*torch.pi*self.input_layers(x))
+        return y
+
+net = Net(n_basis_func, args.scale)
+
+# %%
+# Read test data from csv file
+data = pd.read_csv('Lshape.csv')
+data_cleaned = data[data['U'] != 0 ]
+x_test =  torch.tensor(data_cleaned['X'].values)
+y_test = torch.tensor(data_cleaned['Y'].values)
+X = torch.stack([x_test, y_test], dim=1)
+u_domain_exact = torch.tensor(data_cleaned['U'].values).unsqueeze(1)
+X.requires_grad_(True)
+
+# %%
+# Calculate first and second order derivatives
+dx = []
+dy = []
+dxx = []
+dyy = []
+for i in range(n_basis_func):
+    d = torch.autograd.grad(
+        net(X)[:,i : i + 1], 
+        X, 
+        grad_outputs=torch.ones_like(net(X)[:, i : i + 1]), 
+        create_graph=True)[0]   
+    dx.append(d[:,0:1].detach())      
+    dy.append(d[:,1:2].detach())
+
+    # The second derivative with respect to x
+    dx2 = torch.autograd.grad(d[:,0:1], 
+                               X, 
+                               grad_outputs=torch.ones_like(d[:,0:1]), 
+                               create_graph=True)[0]
+    dxx.append(dx2[:,0:1].detach())
+    
+    # The second derivative with respect to y
+    dy2 = torch.autograd.grad(d[:,1:2], 
+                               X, 
+                               grad_outputs=torch.ones_like(d[:,1:2]), 
+                               create_graph=True)[0]
+    dyy.append(dy2[:,1:2].detach())
+
+dx = torch.cat(dx,dim=1)
+dy = torch.cat(dy,dim=1) # [n，m]
+dxx = torch.cat(dxx,dim=1)
+dyy = torch.cat(dyy,dim=1) # [n，m]
+
+# %%
+# set initial condition
+size_x_initial = 200
+x_initial = np.concatenate([np.linspace(-1, -1, size_x_initial).reshape(-1,1), 
+                            np.linspace(-1, 1, size_x_initial).reshape(-1,1)], axis=1)
+x_initial = torch.from_numpy(x_initial)
+
+# Set boundary condition
+size_x_bound = 200
+
+x_bound1 = np.concatenate([np.linspace(-1, 1, size_x_bound).reshape(-1,1), 
+                           np.linspace(-1, -1, size_x_bound).reshape(-1,1)], axis=1)
+x_bound1 = torch.from_numpy(x_bound1)
+
+x_bound2 = np.concatenate([np.linspace(-1, 0, size_x_bound).reshape(-1,1), 
+                           np.linspace(1, 1, size_x_bound).reshape(-1,1)], axis=1)
+x_bound2 = torch.from_numpy(x_bound2)
+
+x_bound3 = np.concatenate([np.linspace(0, 1, size_x_bound).reshape(-1,1), 
+                           np.linspace(0, 0, size_x_bound).reshape(-1,1)], axis=1)
+x_bound3 = torch.from_numpy(x_bound3)
+
+x_bound4 = np.concatenate([np.linspace(0, 0, size_x_bound).reshape(-1,1), 
+                           np.linspace(0, 1, size_x_bound).reshape(-1,1)], axis=1)
+x_bound4 = torch.from_numpy(x_bound4)
+
+x_bound5 = np.concatenate([np.linspace(1, 1, size_x_bound).reshape(-1,1), 
+                           np.linspace(-1, 0, size_x_bound).reshape(-1,1)], axis=1)
+x_bound5 = torch.from_numpy(x_bound5)
+
+X_bound = torch.cat((x_initial, x_bound1, x_bound2, x_bound3, x_bound4, x_bound5), dim=0)
+
+# %%
+f_domain_exact = np.ones((X.shape[0],1))
+
+# boundary condition
+u_bound_pred = net(X_bound.to(X.device))
+u_bound_pred = u_bound_pred.detach().cpu().numpy()
+u_bound_exact = np.zeros((X_bound.shape[0],1))
+
+# The interior point conditions of eqution
+u_domain_dxx_pred = dxx.detach().cpu().numpy() 
+u_domain_dyy_pred = dyy.detach().cpu().numpy()  
+
+# AC = f
+A = np.vstack([-u_domain_dxx_pred -u_domain_dyy_pred , u_bound_pred])
+f = np.vstack([f_domain_exact, u_bound_exact])
+
+# %%
+# Perform least-squares approximation to obtain coefficient matrix C
+C = sci.linalg.lstsq(A, f)[0] # (M，1)  
+
+# %%
+# Calculate Metrics
+u_pred = (net(X).detach().cpu().numpy() @ C) # Calculate predicted values
+u_domain_exact = u_domain_exact.detach().cpu().numpy() # Calculate the ground truth value of collection points
+
+# Calculate relative error
+rel_error = np.linalg.norm(u_pred-u_domain_exact, 2) / np.linalg.norm(u_domain_exact, 2)
+print("Relative Error:", rel_error)
+
+# Calculate max error
+Max_error = max_error(u_pred, u_domain_exact)
+print("Max Error:", Max_error)
+
+# %%
+# Record execution time
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"Execution time: {execution_time}s")
+
+# %%
+# Save basis/feature functions and coefficient matrix C
+if not os.path.exists(args.save_dir):
+    os.makedirs(args.save_dir)
+torch.save(net.state_dict(), os.path.join(args.save_dir, f"network_{args.num_basis}basis_scale{args.scale}_seed{args.seed}.pth"))
+np.save(os.path.join(args.save_dir, f"C_{args.num_basis}basis_scale{args.scale}_seed{args.seed}.npy"), C)
